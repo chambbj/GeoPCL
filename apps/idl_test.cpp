a934b6d657d9cdb3f4d79287ccbf8fe14c2444f9
Bradley J Chambers <brad.chambers@gmail.com>
2012-06-25 21:18:45 -0400

    basic structure for interface to IDL

---
+++ apps/idl_test.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "idl_export.h"
+
+#include <iostream>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+#include <liblas/liblas.hpp>
+
+#include <geopcl/io/LAStoPCD.hpp>
+
+int
+sum_array_natural(IDL_STRING *input, IDL_STRING *output)
+{ 
+  std::cout << "Reading " << input << " and writing " << output << std::endl;
+
+  pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+
+  liblas::Header header;
+  geopcl::LAStoPCD(input, header, *cloud);
+
+  pcl::io::savePCDFileASCII(output.c_str(), *cloud);
+
+  return 0;
+}
+
+int
+sum_array(int argc, void *argv[])
+{
+  if (argc != 3)
+  {
+    std::cerr << "Required arguments: input.las output.pcd" << std::endl;
+    return 1;
+  }
+
+  return sum_array_natural((IDL_STRING *) argv[0], (IDL_STRING *) argv[1]);
+}
+
