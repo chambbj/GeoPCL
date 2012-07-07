7f20484182b33631b54b11828af7e7c0907c1aee
Bradley J Chambers <brad.chambers@gmail.com>
2012-07-07 12:16:05 -0400

    add convex option as default

--- apps/idl_hull.cpp
+++ apps/idl_hull.cpp
@@ -6,32 +6,52 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/surface/concave_hull.h>
+#include <pcl/surface/convex_hull.h>
 
 #include <liblas/liblas.hpp>
 
 #include <geopcl/io/LAStoPCD.hpp>
+#include <geopcl/io/PCDtoLAS.hpp>
 
 int
 idl_hull_natural(IDL_STRING *input, IDL_STRING *output)
 { 
-  std::cout << "Reading " << input << " and writing " << output << std::endl;
+  std::cout << "Reading " << IDL_STRING_STR(input) << " and writing " << IDL_STRING_STR(output) << std::endl;
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
 
   liblas::Header header;
   geopcl::LAStoPCD(IDL_STRING_STR(input), header, *cloud);
 
-  // Create the concave hull
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_hull (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::ConcaveHull<pcl::PointXYZ> chull;
-  chull.setInputCloud (cloud);
-  chull.setAlpha (0.1);
-  chull.reconstruct (*cloud_hull);
+  bool concave = false;
 
-  std::cerr << "Concave hull has: " << cloud_hull->points.size()
-            << " data points." << std::endl;
+  if(concave)
+  {
+    // Create the concave hull
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_hull (new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::ConcaveHull<pcl::PointXYZI> chull;
+    chull.setInputCloud (cloud);
+    chull.setAlpha (0.1);
+    chull.reconstruct (*cloud_hull);
 
-  pcl::io::savePCDFileASCII(IDL_STRING_STR(output), *cloud_hull);
+    std::cerr << "Concave hull has: " << cloud_hull->points.size()
+              << " data points." << std::endl;
+
+    geopcl::PCDtoLAS(IDL_STRING_STR(output), header, *cloud_hull);
+  }
+  else
+  {
+    // Create the convex hull
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_hull (new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::ConvexHull<pcl::PointXYZI> chull;
+    chull.setInputCloud (cloud);
+    chull.reconstruct (*cloud_hull);
+
+    std::cerr << "Convex hull has: " << cloud_hull->points.size()
+              << " data points." << std::endl;
+
+    geopcl::PCDtoLAS(IDL_STRING_STR(output), header, *cloud_hull);
+  }
 
   return 0;
 }
@@ -39,7 +59,7 @@ idl_hull_natural(IDL_STRING *input, IDL_STRING *output)
 int
 idl_hull(int argc, void *argv[])
 {
-  if (argc != 3)
+  if (argc != 2)
   {
     std::cerr << "Required arguments: input.las output.pcd" << std::endl;
     return 1;
